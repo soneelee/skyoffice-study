05f0a61effced1baeccc5c4746dc8e46b6c44c96
suyeon woo <woosean999@gmail.com>
2023-01-01 21:09:29 +0900

    메인스크린 언마운트시 유저 카운트 -1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -38,6 +38,24 @@ async function getData(url = '') {
   return response.json()
 }
 
+// POST 메서드 구현 예제
+async function postData(url = '', data = {}) {
+  // 옵션 기본 값은 *로 강조
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE 등
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body의 데이터 유형은 반드시 "Content-Type" 헤더와 일치해야 함
+  })
+  return response.json() // JSON 응답을 네이티브 JavaScript 객체로 파싱
+}
+
 function App() {
   const loggedIn = useAppSelector((state) => state.user.loggedIn)
   const computerDialogOpen = useAppSelector((state) => state.computer.computerDialogOpen)
@@ -50,6 +68,12 @@ function App() {
     getData('http://localhost:2567/api/get/user_cnt').then((data) => {
       setUserCnt(data?.userCnt || 0)
     })
+
+    return () => {
+      postData('http://localhost:2567/api/post/user_cnt', { diff: -1 }).then((data) => {
+        console.log(data) // JSON 데이터가 `data.json()` 호출에 의해 파싱됨
+      })
+    }
   }, [])
 
   let ui: JSX.Element
